69e4cc39d2cc56f55484122bcc9f61303f38e0a8
adeab <adeab@live.com>
2019-10-19 18:04:01 +0600

    noaccess

--- resources/views/pages/noaccess.blade.php
+++ resources/views/pages/noaccess.blade.php
@@ -1,7 +1,64 @@
-@extends('layouts.admin')
-@section('page_title')
-Admin Dashboard    
-@endsection
-@section('body_content')
-No Access
-@endsection
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>agaiye cholo</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<script src="https://kit.fontawesome.com/beffab7788.js"></script>
+
+	<style type="text/css">
+		body {
+    background: #dedede;
+}
+.page-wrap {
+    min-height: 90vh;
+}
+.img_404_logo{
+	height: 50px;
+	width: 124px;
+	margin: 0 auto;
+}
+.img_404_logo img{
+	height: 100%;
+	width: 100%;
+	
+}
+.btn-link {
+    font-weight: 400;
+    color: #000;
+	text-decoration: none;
+	border: 2px solid #d54444;
+}
+.btn-link:hover {
+	background:#d54444;
+	color: #fff;
+	text-decoration: none;
+	}
+	</style>
+</head>
+<body>
+<div class="img_404_logo">
+	<img src="Logo Basic.png">
+</div>
+<div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+
+                <span class="display-1 d-block">
+                404</span>
+                <div class="mb-4 lead">The page you are looking for was not found.</div>
+                <a href="#" class="btn btn-link">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
